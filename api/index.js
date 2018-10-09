8aa4fcfde9e9757db1d246832c26db2da1ef0ea4
Micah Bales <micahbales@gmail.com>
2018-10-08 20:08:43 -0400

    Keep Algolia Apps Index Synced with Our Database
    * Added Algolia Credentials
    * On loading the application, clear data from the 'apps' index and replace it with whatever App records we have in the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const algoliasearch = require('algoliasearch');
+const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
+const chunk = require('lodash').chunk;
 
 // Connect to our database
 const mongoose = require('mongoose');
@@ -10,9 +13,24 @@ mongoose.connection.on('error', (err) => {
 // Import models
 require('./models/App');
 
-const app = require('./app');
+// Get app data from DB
+const App = mongoose.model('App');
+App.find().exec().then((apps) => {
+  
+  // Sync Algolia search index
+  const index = algoliaClient.initIndex('apps');
+  index.clearIndex(() => {
+    const chunks = chunk(apps, 1000);
+    chunks.map(function(batch) {
+      return index.addObjects(batch);
+    });
+  });
+  
+  // Run API
+  const app = require('./app');
 
-app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
-    console.log(`Express is now running on port ${process.env.PORT}`);
+  app.set('port', process.env.PORT || 7777);
+  const server = app.listen(app.get('port'), () => {
+      console.log(`Express is now running on port ${process.env.PORT}`);
+  });
 });
