0195666e1f428c150f4d8f3150f977a5c68d45e5
Micah Bales <micahbales@gmail.com>
2018-10-08 22:25:05 -0400

    Allow Faceting By Category
    * Added Bootstrap and did some very light styling
    * Added option to filter results by category

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import {Component} from 'inferno';
 import './registerServiceWorker';
-import Logo from './logo';
+import Logo from './Logo';
 import './App.css';
 import {initDevTools} from 'inferno-devtools';
 import algoliasearch from 'algoliasearch';
 import algoliasearchHelper from 'algoliasearch-helper';
 import * as $ from 'jquery';
 var client = algoliasearch('72IDPMKWKA', 'bbedffb18bcdaf7ea43a1db0bcbc7868');
-var helper = algoliasearchHelper(client, 'apps');
+var helper = algoliasearchHelper(client, 'apps', {
+  facets: ['category']
+});
 initDevTools();
 
 function renderHits(content) {
+  renderFacetList(content);
   $('#container').html(function() {
     return $.map(content.hits, function(hit) {
       return '<li>' + hit.name + '</li>';
     });
   });
 }
+
+function renderFacetList(content) {
+  $('#facet-list').html(function() {
+    return $.map(content.getFacetValues('category'), function(facet) {
+      var checkbox = $('<input type=checkbox>')
+        .data('facet', facet.name)
+        .attr('id', 'fl-' + facet.name);
+      if(facet.isRefined) checkbox.attr('checked', 'checked');
+      var label = $('<label>').html(facet.name + ' (' + facet.count + ')')
+                              .attr('for', 'fl-' + facet.name);
+      return $('<li>').append(checkbox).append(label);
+    });
+  });
+}
+
 
 class App extends Component {
 
@@ -30,6 +48,12 @@ class App extends Component {
             .search();
     });
 
+    $('#facet-list').on('click', 'input[type=checkbox]', function(e) {
+      var facetValue = $(this).data('facet');
+      helper.toggleFacetRefinement('category', facetValue)
+            .search();
+    });
+
     helper.search();
   }
 
@@ -39,11 +63,18 @@ class App extends Component {
         <header className="App-header">
           <Logo width="80" height="80" />
           <h1>Quick Search</h1>
+          <input type="text" autocomplete="off" id="search-box" 
+              placeholder="Enter your query here"/>
         </header>
-        <p className="App-intro">
-          <input type="text" autocomplete="off" id="search-box"/>
-          <div id="container"></div>
-        </p>
+        <div className="App-intro row">
+          <div className="col-12 col-sm-5 offset-sm-1">
+            <div id="facet-list"></div>
+          </div>
+
+          <div className="col-12 col-sm-5 ">
+            <div id="container"></div>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { render } from 'inferno';
 import App from './App';
 import './index.css';
